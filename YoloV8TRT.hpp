dfde936033d1c54b19f03f671a2041e490529436
Francesco Oliva <olibartfast@gmail.com>
2023-07-09 19:41:40 +0200

    working on trt engine serialized model loading

--- YoloV8TRT.hpp
+++ YoloV8TRT.hpp
@@ -2,12 +2,25 @@
 #include "common.hpp"
 #include <NvInfer.h>  // for TensorRT API
 #include <cuda_runtime_api.h>  // for CUDA runtime API
-#include <memory>  // for smart pointers
+#include <fstream>
+
+class Logger : public nvinfer1::ILogger
+{
+public:
+    void log(Severity severity, const char* msg) noexcept override
+    {
+        // Implement logging behavior here, e.g., print the log message
+        if (severity != Severity::kINFO)
+        {
+            std::cout << "TensorRT Logger: " << msg << std::endl;
+        }
+    }
+};
 
 class YoloV8TRT : public YoloV8
 {
 private:
-    std::shared_ptr<nvinfer1::ICudaEngine> engine_;
+    std::shared_ptr<nvinfer1::ICudaEngine> engine_{nullptr};
     nvinfer1::IExecutionContext* context_{nullptr};
     std::vector<void*> buffers_;
     std::vector<cv::Size> input_shapes_;
@@ -18,40 +31,38 @@ public:
     // Constructor
     YoloV8TRT(const std::string& engine_path)
     {
-        // // Create TensorRT runtime and deserialize the engine
-        // runtime_ = nvinfer1::createInferRuntime(gLogger);
-        // std::ifstream engine_file(engine_path, std::ios::binary);
-        // if (!engine_file)
-        // {
-        //     throw std::runtime_error("Failed to open engine file: " + engine_path);
-        // }
-        // engine_file.seekg(0, std::ios::end);
-        // size_t file_size = engine_file.tellg();
-        // engine_file.seekg(0, std::ios::beg);
-        // std::vector<char> engine_data(file_size);
-        // engine_file.read(engine_data.data(), file_size);
-        // engine_file.close();
-        // engine_ = std::shared_ptr<nvinfer1::ICudaEngine>(
-        //     runtime_->deserializeCudaEngine(engine_data.data(), file_size),
-        //     [](nvinfer1::ICudaEngine* engine) { engine->destroy(); });
+        Logger logger;
+        runtime_ = nvinfer1::createInferRuntime(logger);
+        std::ifstream engine_file(engine_path, std::ios::binary);
+        if (!engine_file)
+        {
+            throw std::runtime_error("Failed to open engine file: " + engine_path);
+        }
+        engine_file.seekg(0, std::ios::end);
+        size_t file_size = engine_file.tellg();
+        engine_file.seekg(0, std::ios::beg);
+        std::vector<char> engine_data(file_size);
+        engine_file.read(engine_data.data(), file_size);
+        engine_file.close();
+        engine_ = std::shared_ptr<nvinfer1::ICudaEngine>(
+            runtime_->deserializeCudaEngine(engine_data.data(), file_size),
+            [](nvinfer1::ICudaEngine* engine) { engine->destroy(); });
 
-        // // Create execution context and allocate input/output buffers
-        // context_ = engine_->createExecutionContext();
-        // int num_bindings = engine_->getNbBindings();
-        // buffers_.resize(num_bindings);
-        // for (int i = 0; i < num_bindings; ++i)
-        // {
-        //     nvinfer1::Dims dims = engine_->getBindingDimensions(i);
-        //     size_t size = engine_->getBindingSize(i);
-        //     cudaMalloc(&buffers_[i], size);
-        //     if (engine_->bindingIsInput(i))
-        //     {
-        //         input_shapes_.emplace_back(dims.d[2], dims.d[1]);
-        //         input_width_ = dims.d[2];
-        //         input_height_ = dims.d[1];
-        //         channels_ = dims.d[0];
-        //     }
-        // }
+        // Create execution context and allocate input/output buffers
+        context_ = engine_->createExecutionContext();
+        int num_bindings = engine_.get()->getNbBindings();
+        buffers_.resize(num_bindings);
+        for (int i = 0; i < num_bindings; ++i)
+        {
+            nvinfer1::Dims dims = engine_.get()->getBindingDimensions(i);
+            if (engine_.get()->bindingIsInput(i))
+            {
+                input_shapes_.emplace_back(dims.d[2], dims.d[1]);
+                input_width_ = dims.d[2];
+                input_height_ = dims.d[1];
+                channels_ = dims.d[0];
+            }
+        }
     }
 
     // Destructor
