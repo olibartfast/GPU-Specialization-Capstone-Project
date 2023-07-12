cd68faec1553da7dcd788f0cde881973a6903feb
Francesco Oliva <olibartfast@gmail.com>
2023-07-12 03:08:59 +0200

    trt code refactoring

--- YoloV8TRT.hpp
+++ YoloV8TRT.hpp
@@ -28,6 +28,70 @@ private:
     nvinfer1::IRuntime* runtime_{nullptr};
 
 public:
+    // Create the TensorRT runtime and deserialize the engine
+    std::shared_ptr<nvinfer1::ICudaEngine> createRuntimeAndDeserializeEngine(const std::string& engine_path, Logger& logger, nvinfer1::IRuntime*& runtime)
+    {
+        // Create TensorRT runtime
+        runtime = nvinfer1::createInferRuntime(logger);
+
+        // Load engine file
+        std::ifstream engine_file(engine_path, std::ios::binary);
+        if (!engine_file)
+        {
+            throw std::runtime_error("Failed to open engine file: " + engine_path);
+        }
+        engine_file.seekg(0, std::ios::end);
+        size_t file_size = engine_file.tellg();
+        engine_file.seekg(0, std::ios::beg);
+        std::vector<char> engine_data(file_size);
+        engine_file.read(engine_data.data(), file_size);
+        engine_file.close();
+
+        // Deserialize engine
+        std::shared_ptr<nvinfer1::ICudaEngine> engine(
+            runtime->deserializeCudaEngine(engine_data.data(), file_size),
+            [](nvinfer1::ICudaEngine* engine) { engine->destroy(); });
+
+        return engine;
+    }
+
+    // Create execution context and allocate input/output buffers
+    void createContextAndAllocateBuffers(nvinfer1::ICudaEngine* engine, nvinfer1::IExecutionContext*& context, std::vector<void*>& buffers, std::vector<nvinfer1::Dims>& output_dims, std::vector<std::vector<float>>& h_outputs)
+    {
+        context = engine->createExecutionContext();
+        buffers.resize(engine->getNbBindings());
+        for (int i = 0; i < engine->getNbBindings(); ++i)
+        {
+            nvinfer1::Dims dims = engine->getBindingDimensions(i);
+            auto binding_size = getSizeByDim(engine->getBindingDimensions(i)) * sizeof(float);
+            cudaMalloc(&buffers[i], binding_size);
+            if (engine->bindingIsInput(i))
+            {
+                const auto input_shape = std::vector{ dims.d[0], dims.d[1], dims.d[2], dims.d[3] };
+                input_width_ = dims.d[3];
+                input_height_ = dims.d[2];
+                channels_ = dims.d[1];
+            }
+            else
+            {
+                output_dims.emplace_back(engine->getBindingDimensions(i));
+                auto size = getSizeByDim(dims);
+                h_outputs.emplace_back(std::vector<float>(size));
+            }
+        }
+    }
+
+    void initializeBuffers(const std::string& engine_path)
+    {
+        // Create logger
+        Logger logger;
+
+        // Create TensorRT runtime and deserialize engine
+        engine_ = createRuntimeAndDeserializeEngine(engine_path, logger, runtime_);
+
+        // Create execution context and allocate input/output buffers
+        createContextAndAllocateBuffers(engine_.get(), context_, buffers_, output_dims_, h_outputs_);
+    }
 
     // calculate size of tensor
     size_t getSizeByDim(const nvinfer1::Dims& dims)
@@ -43,46 +107,7 @@ public:
 
     YoloV8TRT(const std::string& engine_path)
     {
-        Logger logger;
-        runtime_ = nvinfer1::createInferRuntime(logger);
-        std::ifstream engine_file(engine_path, std::ios::binary);
-        if (!engine_file)
-        {
-            throw std::runtime_error("Failed to open engine file: " + engine_path);
-        }
-        engine_file.seekg(0, std::ios::end);
-        size_t file_size = engine_file.tellg();
-        engine_file.seekg(0, std::ios::beg);
-        std::vector<char> engine_data(file_size);
-        engine_file.read(engine_data.data(), file_size);
-        engine_file.close();
-        engine_ = std::shared_ptr<nvinfer1::ICudaEngine>(
-            runtime_->deserializeCudaEngine(engine_data.data(), file_size),
-            [](nvinfer1::ICudaEngine* engine) { engine->destroy(); });
-
-        // Create execution context and allocate input/output buffers
-        context_ = engine_->createExecutionContext();
-        buffers_.resize(engine_->getNbBindings()); // buffers for input and output data
-        for (int i = 0; i < engine_->getNbBindings(); ++i)
-        {
-            nvinfer1::Dims dims = engine_.get()->getBindingDimensions(i);
-            auto binding_size = getSizeByDim(engine_->getBindingDimensions(i)) * sizeof(float);
-            cudaMalloc(&buffers_[i], binding_size);
-            if (engine_.get()->bindingIsInput(i))
-            {
-                const auto input_shape = std::vector{dims.d[0], dims.d[1], dims.d[2], dims.d[3]};
-                input_width_ = dims.d[3];
-                input_height_ = dims.d[2];
-                channels_ = dims.d[1];
-            }
-            else
-            {
-                output_dims_.emplace_back(engine_->getBindingDimensions(i));
-                auto size = getSizeByDim(dims);
-                h_outputs_.emplace_back(std::vector<float>(size));
-
-            }           
-        }
+        initializeBuffers(engine_path);
     }
 
     // Destructor
