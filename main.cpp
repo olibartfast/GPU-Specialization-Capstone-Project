9356276088100143cc862a2b5b9abe215d161ad2
Francesco Oliva <olibartfast@gmail.com>
2023-07-13 23:56:05 +0200

    Added info on fps

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "cxxopts.hpp"
 #include "common.hpp"
+#include <chrono>
 
 // Include the appropriate header based on the selected framework
 #ifdef USE_ONNX_RUNTIME
@@ -80,6 +81,44 @@ int main(int argc, char* argv[]) {
         "trash_wood"
     };    
 
+
+    // Extract the extension from the weight file path
+    std::string backend;
+    std::string weight_file_extension = result["weights"].as<std::string>();
+    size_t extension_pos = weight_file_extension.rfind('.');
+    if (extension_pos != std::string::npos)
+    {
+        std::string extension = weight_file_extension.substr(extension_pos + 1);
+        std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+
+        if (extension == "onnx")
+        {
+            // Use ONNX Runtime as the backend
+            backend = "ONNX_RUNTIME";
+        }
+        else if (extension == "pt" || extension == "pth" || extension == "torchscript")
+        {
+            // Use LibTorch as the backend
+            backend = "LIBTORCH";
+        }
+        else if (extension == "trt" || extension == "engine" || extension == "plan")
+        {
+            // Use TensorRT as the backend
+            backend = "TENSORRT";
+        }
+        else
+        {
+            std::cerr << "Invalid weight file extension. Supported extensions: .onnx, .pt, .pth, .trt" << std::endl;
+            std::exit(1);
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid weight file path" << std::endl;
+        std::exit(1);
+    }
+
+
     std::unique_ptr<YoloV8> yolo = createYoloV8(weights,is_gpu);
     if (!yolo) {
         std::cerr << "Invalid framework specified. Supported frameworks are ONNX_RUNTIME, LIBTORCH, and TENSORRT." << std::endl;
@@ -99,8 +138,15 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 //#endif
+
+    // Variables for FPS calculation
+    int frame_count = 0;
+    double total_time = 0.0;
+    double average_fps = 0.0;
+
     while(cap.read(frame))
     {
+         auto start_time = std::chrono::steady_clock::now();
         const auto detections = yolo->infer(frame);
         cv::Mat frame_with_mask = frame.clone();
         for (int i = 0; i < detections.size(); ++i) 
@@ -115,16 +161,45 @@ int main(int argc, char* argv[]) {
             int baseline = 0;
             cv::Size textSize = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseline);
             cv::Point textOrg(detections[i].bbox.x, detections[i].bbox.y - textSize.height);
-            cv::putText(frame, text, textOrg, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
+            cv::putText(frame, text, textOrg, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 0), 2, cv::LINE_AA);
         }
         cv::addWeighted(frame, 0.5, frame_with_mask, 0.5, 0, frame);
+
+        // Calculate inference time and FPS
+        // Measure the time taken for inference
+        auto end_time = std::chrono::steady_clock::now();
+        double frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() / 1000.0;
+
+        // Compute FPS
+        double fps = 1.0 / frame_time;
+
+        // Accumulate frame time and count
+        total_time += frame_time;
+        frame_count++;
+
+        // Compute average FPS
+        average_fps = frame_count / total_time;
+
+        // Print FPS on the frame
+        std::string fps_text = "FPS: " + std::to_string(fps);
+        cv::putText(frame, fps_text, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(255, 255, 255), 2);
+
+        // Print backend, model name, and GPU flag on the frame
+        std::string backend_text = "Backend: " + backend;
+        std::string gpu_text = "GPU: " + std::string(is_gpu ? "true" : "false");
+        cv::putText(frame, backend_text, cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(255, 255, 255), 2);
+        cv::putText(frame, gpu_text, cv::Point(10, 90), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(255, 255, 255), 2);
+
         cv::imshow("", frame);
         cv::waitKey(1);
-//#ifdef WRITE_FRAME
+        //#ifdef WRITE_FRAME
         outputVideo.write(frame);
-//#endif        
-
+        //#endif
     }
+
+    // Print the average FPS
+    std::cout << "Average FPS: " << average_fps << std::endl;
+
 
 
     return 0;
